9461f42284c988b1e9397a1b019ac8c241b3a214
Guillermo Casanova <guillermoluisc@gmail.com>
2022-11-07 15:49:49 -0300

    Intro-uno-js : este es mi primer ejercicio en JS

---
+++ index.js
@@ -0,0 +1,10 @@
+let nombre_ciudad=`Chacabuco`
+console.log(`Antes del cambio el valor de la variable nombre_ciudad tiene el valor= `+nombre_ciudad)
+let numero_random=Math.floor(Math.random()*10)
+nombre_ciudad=numero_random
+console.log(`Numero random generado aleatoriamente: `+numero_random)
+console.log(`Después del cambio el valor de la variable nombre_ciudad tiene el valor= `+nombre_ciudad)
+const nombre=`Guillermo Luis`
+const apellido=`Casanova`
+const nombre_completo=nombre+' '+apellido
+console.log(nombre_completo)
