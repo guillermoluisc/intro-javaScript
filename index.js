16ee43256626beaa6d92bf418e95b2185e35061e
Guillermo Casanova <guillermoluisc@gmail.com>
2022-11-09 12:20:05 -0300

    Intro-uno-js : Corrección template string

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ console.log(`Numero random generado aleatoriamente: `+numero_random)
 console.log(`Después del cambio el valor de la variable nombre_ciudad tiene el valor= `+nombre_ciudad)
 const nombre=`Guillermo Luis`
 const apellido=`Casanova`
-const nombre_completo=nombre+' '+apellido
+const nombre_completo=`${nombre} ${apellido}`
 console.log(nombre_completo)
