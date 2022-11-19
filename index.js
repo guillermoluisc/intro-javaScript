a6e8e00f6005aa016b1d1f9d18552fb6e7e0590a
Guillermo Casanova <guillermoluisc@gmail.com>
2022-11-18 22:36:46 -0300

    Cambio de 10 por 5-pull Request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let nombre_ciudad=`Chacabuco`
 console.log(`Antes del cambio el valor de la variable nombre_ciudad tiene el valor: `+nombre_ciudad)
-let numero_random=Math.floor(Math.random()*10)
+let numero_random=Math.floor(Math.random()*5)
 nombre_ciudad=numero_random
 console.log(`Numero random generado aleatoriamente: `+numero_random)
 console.log(`Después del cambio el valor de la variable nombre_ciudad tiene el valor: `+nombre_ciudad)
